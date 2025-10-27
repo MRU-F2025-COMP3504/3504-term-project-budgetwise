e08e78ae3ece837fffe198588d6caa5e74351348
laurence298 <laurencemunoko@gmail.com>
2025-10-27 17:23:13 -0600

    added Get Statements and Transction endpoints

--- budgetwise/src/app/api/transactions/GET/route.js
+++ budgetwise/src/app/api/transactions/GET/route.js
@@ -1,22 +1,19 @@
 import { NextResponse } from 'next/server';
-import supabase from '@/helpers/databaseConnector';
+import supabase from '../../../../helpers/databaseConnector';
 
-export async function POST(req) {
+export async function GET() {
   try {
-    const body = await req.json();
+    const { data: Transactions, error } = await supabase
+      .from('Transactions')
+      .select('*');
 
-    const { data: userData, error: loginError } = await supabase.auth.signInWithPassword({
-    email: 'test1@gmail.com',
-    password: '12345',
-    });
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 
-    let { data: Transactions, error } = await supabase
-    .from('Transactions')
-    .select('*')
-
-    return NextResponse.json({ Transactions});
+    return NextResponse.json({ Transactions });
   } catch (err) {
-    console.error('❌ Error processing CSV:', err);
+    console.error('❌ Error fetching transactions:', err);
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
