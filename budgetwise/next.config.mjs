7d90b11ac73a02fe4ddccabbdd489dc5ddb95e3a
laurence298 <laurencemunoko@gmail.com>
2025-11-02 13:23:32 -0700

    changed application to static site

--- budgetwise/next.config.mjs
+++ budgetwise/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // ✅ Forces static export
+  reactStrictMode: true,
+};
 
 export default nextConfig;
