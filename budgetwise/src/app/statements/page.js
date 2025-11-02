3430b0b661c8c4c8fdd37324e10c5b4f9a0ebb24
laurence298 <laurencemunoko@gmail.com>
2025-11-02 16:31:45 -0700

    file deleted

--- budgetwise/src/app/statements/page.js
+++ budgetwise/src/app/statements/page.js
@@ -3,7 +3,7 @@ import StatementsPage from "../../FrontEnd/pages/StatementsPage";
 export const dynamic = "force-dynamic"; // always fetch fresh data
 
 export default async function Page() {
-  const baseUrl = process.env.URL 
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
     || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
 
   const res = await fetch(`${baseUrl}/api/statements`, { cache: 'no-store' });
